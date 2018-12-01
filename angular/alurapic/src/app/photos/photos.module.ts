30ea78f97e6165f375a94651f42a9dcf95ab82ed
ridneto <ridineu.moraes@valemobi.com.br>
2018-12-01 18:54:12 -0200

    Angular módulo I

---
+++ angular/alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+
+@NgModule({
+    imports: [ 
+        CommonModule,
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+    ]
+})
+export class PhotosModule {}
